eaa2d3bfdfa2e8f01b85e722154168829ae6f575
zhumz <zhumz@nscc-tj.cn>
2022-06-14 10:03:59 +0800

    single django project work

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ if [ ! -d "$PROJECT_NAME" ]; then
     echo ${PROJECT_NAME};
     django-admin startproject ${PROJECT_NAME} .;
 fi
-python ${PROJECT_NAME}/manager.py 0.0.0.0:${PORT};
+python manage.py runserver 0.0.0.0:${SERVER_PORT};
